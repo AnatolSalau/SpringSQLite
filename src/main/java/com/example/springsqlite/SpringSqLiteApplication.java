29e176346e837e9502cf91f33728024cdd44be9c
AnatolSalau <salovanatoly5@mail.ru>
2023-06-20 15:51:18 +0300

    Initial project

---
+++ src/main/java/com/example/springsqlite/SpringSqLiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqLiteApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SpringSqLiteApplication.class, args);
+      }
+
+}
